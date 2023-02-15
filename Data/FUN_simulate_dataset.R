23b2f756661fe956689e783a4db5c68a2d7b8157
Danielle Remmerswaal <74618798+daniellerem@users.noreply.github.com>
2023-02-15 11:39:08 +0100

    Repaired file

--- Data/FUN_simulate_dataset.R
+++ Data/FUN_simulate_dataset.R
@@ -1,71 +1,33 @@
-##########################################################################################
-
-#                     Script to generate the datasets for the simulations                #
-
-##########################################################################################
-
-options(scipen = 999)                                                     #scientific notation off
-
-nsim   = 1000                                                              #number of simulation iterations
-nsize  = 5000                                                             #sample size of each dataset
-nboot  = 5                                                                #number of bootstrapped datasets per simulated dataset
-nconds = 8                                                                #number of simulation conditions
-
-set.seed(123)
-
-#load function used for the dataset generation
-source("FUN_specify_simconditions.R")                                            
-source("FUN_simulate_dataset.R")
-source("FUN_generate_bootstraps.R")
-
-#load packages necessary for the dataset generation
-library(poLCA) 
-library(confreq) 
-library(dplyr) 
-
-sim_conds  = simconds()                                                          
-simbootdat = vector("list", nconds)
-
-#DATASET GENERATION
-for(i in 1:nconds){                                                             # loop over conditions
-  cat(i)
-  for(j in 1:nsim){                                                             # loop over simulation iterations
-    
-    sim_data = simdata(nsize      = nsize,                                      # generate a simulated dataset
-                       sel_error  = sim_conds[[i]][[1]],
-                       meas_error = sim_conds[[i]][[2]])
-    
-    
-    simbootdat[[i]][[j]] = bootdata(data  = sim_data,                           # bootstraps of the simulated dataset
-                                    nboot = nboot)
-    
+simdata = function(nsize,
+                   sel_error,
+                   meas_error
+){
+  
+  mod1 = poLCA.simdata(N       = nsize,                                         # generate SE part
+                       nclass  = 2,
+                       probs   = sel_error, 
+                       P       = c(0.15, 0.85), 
+                       missval = F)
+  
+  df1 = cbind(mod1$dat,                                                         # store generated data + trueclass
+              trueclass=mod1$trueclass)
+  
+  mod2 = poLCA.simdata(N       = nsize,                                         # generate ME part
+                       nclass  = 3,
+                       probs   = meas_error,
+                       P       = c(0.4,0.35,0.25),
+                       missval = F)
+  
+  df2 = cbind(mod2$dat[,1:ncol(mod2$dat)]+1,                                    # store generated data + trueclass
+              sectors = mod2$trueclass+1) 
+  
+  for(v in 1:ncol(df1)){                                                           # combine meas data into se data
+    to_replace = which(df1[,v] == 2)
+    df1[,v][to_replace] <- df2[,v][to_replace]
   }
+  
+  dffreq = df1[,1:ncol(df1)] %>%                                                  # aggregate data
+    count(Y1, Y2, Y3, Y4, Y5, trueclass)
+  
+  return(dffreq) 
 }
-save(simbootdat, file = "simdata06092022.RData")
-
-
-#EXTRACT SIMULATED POPULATION VALUES
-longdat          = rep(list(vector("list", nsim)),nconds)                        #create empty lists to store the       
-prop_x_nsim_true = rep(list(vector("list", nsim)),nconds)                        #(intermediate) results in
-covar_nsim_true  = rep(list(array(NA,dim = c(4,4,nsim), dimnames=NULL)), nconds)
-prop_true        = vector("list", nconds)
-covar_true       = vector("list", nconds)
-
-
-for(i in 1:nconds){                                                             
-  cat(i)
-  for(j in 1:nsim){     
-    log <- capture.output({ #make sure not all messages are displayed about the pattern frequencies
-      longdat[[i]][[j]]          <-    as.data.frame(fre2dat(simbootdat[[i]][[j]][,c(1:7)]))    #transfer pattern frequencies to longformat dataframe (column 7 contains the pattern frequencies original data)
-    })
-    prop_x_nsim_true[[i]][[j]]   <-  prop.table(table(longdat[[i]][[j]]$trueclass))
-    covar_nsim_true[[i]][,,j]    <-  prop.table(table(longdat[[i]][[j]]$trueclass, longdat[[i]][[j]]$Y5))
-  }#end loop over nsim
-  covar_true[[i]]                <-  apply(covar_nsim_true[[i]], MARGIN = c(1, 2), FUN = mean)   # average over nsim
-  prop_true[[i]]                 <-  colMeans(bind_rows(prop_x_nsim_true[[i]])) 
-}#end loop over nconds
-
-#save(covar_true, file = "SimulatedCovariates.RData")                            #the class size estimates
-#save(prop_true,  file = "SimulatedProportions.RData")                           #the class-covariate estimates
-
-#important: save or move the population values of the generated datasets to the post-processing folder
